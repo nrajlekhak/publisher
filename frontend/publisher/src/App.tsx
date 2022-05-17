e65ea8af62f7dade3ff80a1abed773b3340d135b
leapfrog <leapfrog@pop-os.localdomain>
2022-05-17 11:12:47 +0545

    Remove dotenv from Publisher

--- frontend/publisher/src/App.tsx
+++ frontend/publisher/src/App.tsx
@@ -1,7 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import * as dotenv from "dotenv";
-dotenv.config({ path: __dirname + "/.env" });
 import "./index.scss";
 
 const App = () => (
