6f860877a48b2640191705fbb6c21d11931ffbf8
Neeraj Lekhak <nrajlekhak@gmail.com>
2022-05-17 09:52:57 +0545

    added shared layout for admin and publisher apps

---
+++ frontend/admin/src/pages/Dashboard/index.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import Table from '../../components/Table';
+
+const Dashboard = () => {
+  return <Table title='Articles'></Table>;
+};
+
+export default Dashboard;
--- frontend/publisher/webpack.config.js
+++ frontend/publisher/webpack.config.js
@@ -1,14 +1,14 @@
-const HtmlWebPackPlugin = require("html-webpack-plugin");
-const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPlugin");
+const HtmlWebPackPlugin = require('html-webpack-plugin');
+const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin');
 
-const deps = require("./package.json").dependencies;
+const deps = require('./package.json').dependencies;
 module.exports = {
   output: {
-    publicPath: "http://localhost:3001/",
+    publicPath: 'http://localhost:3001/',
   },
 
   resolve: {
-    extensions: [".tsx", ".ts", ".jsx", ".js", ".json"],
+    extensions: ['.tsx', '.ts', '.jsx', '.js', '.json'],
   },
 
   devServer: {
@@ -20,20 +20,20 @@ module.exports = {
     rules: [
       {
         test: /\.m?js/,
-        type: "javascript/auto",
+        type: 'javascript/auto',
         resolve: {
           fullySpecified: false,
         },
       },
       {
         test: /\.(css|s[ac]ss)$/i,
-        use: ["style-loader", "css-loader", "postcss-loader"],
+        use: ['style-loader', 'css-loader', 'postcss-loader'],
       },
       {
         test: /\.(ts|tsx|js|jsx)$/,
         exclude: /node_modules/,
         use: {
-          loader: "babel-loader",
+          loader: 'babel-loader',
         },
       },
     ],
@@ -41,26 +41,28 @@ module.exports = {
 
   plugins: [
     new ModuleFederationPlugin({
-      name: "publisher",
-      filename: "remoteEntry.js",
+      name: 'publisher',
+      filename: 'remoteEntry.js',
       remotes: {
-        admin: "admin@http://localhost:3001/remoteEntry.js",
+        admin: 'admin@http://localhost:3001/remoteEntry.js',
       },
-      exposes: {},
+      exposes: {
+        './Layout': './src/Layout.tsx',
+      },
       shared: {
         ...deps,
         react: {
           singleton: true,
           requiredVersion: deps.react,
         },
-        "react-dom": {
+        'react-dom': {
           singleton: true,
-          requiredVersion: deps["react-dom"],
+          requiredVersion: deps['react-dom'],
         },
       },
     }),
     new HtmlWebPackPlugin({
-      template: "./src/index.html",
+      template: './src/index.html',
     }),
   ],
 };
